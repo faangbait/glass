d6c8c05fc8dcd936b63f80b253e524d4385a78fe
S Smith <root@madeof.glass>
2022-05-03 01:10:33 -0500

    Titles and slugify

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,15 @@
-
 from django.db import models
+from django.utils.text import slugify
 from martor.models import MartorField
 
 
 class Blog(models.Model):
-    slug = models.CharField(max_length=24, primary_key=True)
     draft = models.BooleanField(default=True)
+    title = models.CharField(max_length=24)
+    slug = models.CharField(max_length=24, primary_key=True, blank=True)
     body = MartorField()
+
+    def save(self, **kwargs):
+        if self.slug is None:
+            self.slug = slugify(self.title)
+        return super().save(kwargs)
